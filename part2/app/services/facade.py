74d8a75b164e6472681de13d8c9405abef79281b
ReemAlshehri04 <re2malshehri@gmail.com>
2026-02-19 19:41:56 +0000

    Update HBnBFacade implementation

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,6 +1,3 @@
-from app.models import User
-from app.persistence.repository import InMemoryRepository
-
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
@@ -8,7 +5,7 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # USER METHODS
+    # -------- USER --------
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
@@ -27,9 +24,50 @@ class HBnBFacade:
         user = self.user_repo.get(user_id)
         if not user:
             return None
-        user.update(data)
+        for key, value in data.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
         return user
 
-    # PLACE METHODS (placeholders for now)
+
+    # -------- AMENITY --------
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.get_amenity(amenity_id)
+        if not amenity:
+            return None
+        if 'name' in amenity_data:
+            amenity.name = amenity_data['name']
+        return amenity
+
+
+    # -------- PLACE --------
+    def create_place(self, place_data):
+        place = Place(**place_data)
+        self.place_repo.add(place)
+        return place
+
     def get_place(self, place_id):
-        pass
+        return self.place_repo.get(place_id)
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, place_data):
+        place = self.get_place(place_id)
+        if not place:
+            return None
+        for key, value in place_data.items():
+            if hasattr(place, key):
+                setattr(place, key, value)
+        return place
