5aa4a73f79b9179cd3b1295ffd1b09dd8fb83a99
ReemAlshehri04 <re2malshehri@gmail.com>
2026-04-08 13:07:58 +0300

    Refactor create_app to merge duplicate definitions and enable CORS

--- part4/app/__init__.py
+++ part4/app/__init__.py
@@ -3,20 +3,26 @@ from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
 
-
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+  
+    CORS(app)
+
+   
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
 
+    
     api = Api(
         app,
         version='1.0',
@@ -25,6 +31,7 @@ def create_app(config_class="config.DevelopmentConfig"):
         doc='/api/v1/'
     )
 
+    
     from app.api.v1.users import api as users_ns
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.places import api as places_ns
